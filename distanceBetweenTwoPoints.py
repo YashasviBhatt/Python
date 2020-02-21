d8f36c6bedc683534d61d2e7908bcbb567168727
YashasviBhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-02-21 20:10:02 +0530

    Add files via upload

---
+++ distanceBetweenTwoPoints.py
@@ -0,0 +1,14 @@
+# Python Program to calculate distance between 2 points
+# For this we use Euclidean Distance
+# This Could have done using either math.sqrt() function to find the square root of the value
+# Or Numpy.Euclidean function could have used for this
+
+print('Enter I Coordinate')
+x1,y1=int(input('Enter x1 : ')),int(input('Enter y1 : '))
+x2,y2=int(input('Enter x2 : ')),int(input('Enter y2 : '))
+
+def Euclidean(x1,y1,x2,y2) :
+    return (((y2-y1)**2+(x2-x1)**2)**(1/2))
+
+d=Euclidean(x1,y1,x2,y2)
+print(d)
---
+++ sumOfTwoNumbers.py
@@ -0,0 +1,11 @@
+# Python Program to Calculate addition of 2 Numbers
+
+num1=int(input('Enter 1st Number : '))
+num2=int(input('Enter 2nd Number : '))
+
+def Add(a,b) :
+    return a+b
+
+total=Add(num1,num2)
+
+print('Addition of {} and {} is {}'.format(num1,num2,total))
