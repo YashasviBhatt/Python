0d55e9e4cfc91ede215e6c14ae93968b710aee7a
YashasviBhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-02-22 09:55:29 +0530

    Update dataTypes.py

--- dataTypes.py
+++ dataTypes.py
@@ -1,18 +1,12 @@
 # Python Program to demonstrate the basic datatypes
-
 a=10
 b='YashasviBhatt'
 c='Yashasvi Bhatt'
-d=['a',6,10,'Hello World',[1,2,3],12.56,True]
-e=('Python',10)
+d=12.56
+e=True
 
 print('1 ',type(a))
 print('2 ',type(b))
 print('3 ',type(c))
 print('4 ',type(d))
 print('5 ',type(e))
-print('6 ',type(d[0]))
-print('7 ',type(d[3]))
-print('8 ',type(d[4][2]))
-print('9 ',type(d[5]))
-print('10 ',type(d[6]))
