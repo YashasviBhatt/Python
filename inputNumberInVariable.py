c01d186e0506ea7c4df56e7a6a62a2a05a568bd1
Yashasvi Bhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-02-23 20:15:33 +0530

    Add files via upload

---
+++ inputNumberInVariable.py
@@ -0,0 +1,11 @@
+#Program to input a Number in a variable using input(function) in python
+
+a=0
+b=0
+c=0
+a=input("Enter Value of a")
+b=input("Enter Value of b")
+c=input("Enter Value of c")
+print("Entered Value of a",a)
+print("Entered Value of b",b)
+print("Entered Value of c",c)
