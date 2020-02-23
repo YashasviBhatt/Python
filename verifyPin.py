02100a7df27b913bf998431d177d028e0c31debc
Yashasvi Bhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-02-23 20:16:41 +0530

    Add files via upload

---
+++ verifyPin.py
@@ -0,0 +1,18 @@
+# Program to verify pin
+
+import time
+
+pin_inputs=[]
+
+for inpt in range(0,3) :
+    i=int(input("Enter your Pin"))
+    pin_inputs.append(inpt)
+    if i==2014 :
+        print("Correct Pin")
+        time.sleep(3)
+        break
+    elif len(pin_inputs) == 3 :
+        print("You have Entered wrong Pin 3 times your card has been blocked")
+        time.sleep(3)
+    else :
+        continue
