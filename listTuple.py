140516185cd3f5ec5679e5a14ec7665a76b931a9
Yashasvi Bhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-02-29 06:03:29 -0800

    Add files via upload

---
+++ listTuple.py
@@ -0,0 +1,8 @@
+list=[12,13,[1,2,3],4,5]        # this is a list
+tuple=(12,13,(1,2,3),4,5)       # this is a tuple
+
+print(type(list))
+print(type(tuple))
+
+#list[3]=5                       # can be done since list is immutable
+#tuple(3)=5                      # can't be done because tuple is mutable
---
+++ loopSequence.py
@@ -0,0 +1,4 @@
+seq_lim=int(input("Enter Sequence Limit : "))
+
+for s in range(1,seq_lim+1) :
+    print(s)
