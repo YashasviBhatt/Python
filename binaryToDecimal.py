c9dca2102784635750b6fe57e49452154c236e9b
Yashasvi Bhatt <51393376+YashasviBhatt@users.noreply.github.com>
2020-03-13 20:34:56 +0530

    Add files via upload

---
+++ binaryToDecimal.py
@@ -0,0 +1,48 @@
+# Program to find the decimal equivalent of n digit binary number
+
+def BinaryToDecimal(bin_num,bin_len):
+    # creating a list enlisted with binary codes of decimal numbers
+    binary_input_list = ["{:b}".format(num) for num in range(0,2**bin_len)]
+    multiplier=1
+    dec_eqvlnt=0
+
+    # checking that the input binary number should contain max 4 digits
+    if len(str(bin_num)) > bin_len :
+        print('Binary Number Inserted Should be of 4 digits maximum')
+        exit()
+    # checking whether the entered binary os valid or not
+
+    flag=0
+    for num in binary_input_list :
+        if str(bin_num)==num :
+            flag=1
+    if flag==0 :
+        print('Invalid Binary Entered')
+        exit()
+    # calculating the binary equivalent
+
+    else :
+        while bin_num>=1 :
+            dec_eqvlnt+=int(bin_num%10*multiplier)
+            bin_num=int(bin_num/10)
+            multiplier*=2
+
+        return dec_eqvlnt
+
+bin_len=int(input('Enter the no of digits you want to insert (max 10)'))
+# ensuring that the inserted binary should not be greater than 10
+# since it will be so much heavy for program
+if bin_len>10 :
+    print('Value too Large (insert value less than or equals to 10)')
+    exit()
+
+binary_input=int(input('Enter a Binary Number of max {} digits'.format(bin_len)))
+# calling the BinaryToDecimal function that returns a value to a variable
+
+binary_eqvlnt=BinaryToDecimal(binary_input,bin_len)
+print('The Binary Equivalent of {} is {}'.format(binary_input,binary_eqvlnt))
+
+# or directly you can use bin function in python which will return bin equivalent of number
+# a=int(input('Enter Binary'))
+# print(bin(a))  // it will return binary with 0b at the starting
+# print({:b}.format(a))   // it will return binary without 0b at starting
