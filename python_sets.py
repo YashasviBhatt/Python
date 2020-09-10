8f090153c63a42d31969c9ebdbc52646c0bb7fbc
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-09-10 17:03:33 +0530

    Add files via upload

---
+++ python_sets.py
@@ -0,0 +1,13 @@
+s = {1, 1, 2, 3, 4}
+print(s)
+print(type(s))
+
+# set will store only distinct value and ignore duplicate values
+
+
+# typecasting to set
+
+lst = [1, 2, 3, 4]
+st = set(lst)
+
+print(type(st))
