7b1ebc3c336f94550613ff7b1cc95fab00da8ea8
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-10-03 10:00:29 +0530

    Python

--- meanStandardDeviation.py
+++ meanStandardDeviation.py
@@ -1,4 +1,5 @@
 from random import randint
+import statistics as stats
 
 lower_limit = int(input('Enter Lower Limit : '))
 upper_limit = int(input('Enter Upper Limit : '))
@@ -8,4 +9,8 @@ values = list()
 for num in range(num_of_values):
     values.append(randint(lower_limit, upper_limit))
 
-print(values)
+mean = stats.mean(values)
+
+standard_deviation = stats.stdev(values)
+
+print([mean, standard_deviation])
