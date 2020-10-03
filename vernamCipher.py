ee47fe507cbe10005225c43bb8e1f39c7d0a2b6e
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-10-03 11:33:12 +0530

    Python

--- vernamCipher.py
+++ vernamCipher.py
@@ -1,20 +1,27 @@
 # Function Vernam Cipher
 # Created by Yashasvi Bhatt
 
-def VernamCipher(input_message,one_time_pad) :
-    if len(input_message) == len(one_time_pad) :
-        print("Cipher Text : ",end='')
-        for i,o in zip(input_message,one_time_pad) :
-            a=ord(i)+ord(o)                             # ord is used to find the ascii value of a character
-            if a>219 :
-                a=a-123
-                print(chr(a),end='')                    # chr is used to fetch the character from its ascii value
-            else :
-                print(chr(a-97),end='')
-    else :
+def VernamCipher(input_message, one_time_pad):
+    output = ''
+    if len(input_message) == len(one_time_pad):
+        for i,o in zip(input_message, one_time_pad):
+            if i == ' ' and o == ' ':
+                output += ' '
+                continue
+            elif i == ' ' and o != ' ' or i != ' ' and o == ' ':
+                print('Invalid Inputs')
+                break
+            a = ord(i) + ord(o)                         # ord is used to find the ascii value of a character
+            if a > 219:
+                a = a - 123
+                output += chr(a)                        # chr is used to fetch the character from its ascii value
+            else:
+                output += chr(a - 97)
+        return output
+    else:
         print("Length of PT and OTP must be same")
 
 input_message=input("Enter Plain Text : ")
 one_time_pad=input("Enter One Time Pad : ")
 
-VernamCipher(input_message,one_time_pad)
+print("Cipher Text : ", VernamCipher(input_message, one_time_pad))
